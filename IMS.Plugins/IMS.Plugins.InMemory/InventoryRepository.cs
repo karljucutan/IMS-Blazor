c969363e866e41dad36dca234cf99c5938beadd9
Karl Louis T. Jucutan <kltjucutan@live.mcl.edu.ph>
2024-07-02 19:28:25 +0800

    [Inventory List] - Added properties in Inventroy Entity. Created a plugin/persistence that store in In-Memory.

---
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.Plugins.InMemory
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        private List<Inventory> _inventories;
+
+        public InventoryRepository() 
+        {
+            _inventories = new()
+            {
+                new Inventory { InventoryId = 1, InventoryName = "Bike Seat", Quantity = 10, Price = 2 },
+                new Inventory { InventoryId = 2, InventoryName = "Bike Body", Quantity = 10, Price = 2 },
+                new Inventory { InventoryId = 3, InventoryName = "Bike Wheels", Quantity = 10, Price = 2 },
+                new Inventory { InventoryId = 4, InventoryName = "Bike Pedals", Quantity = 10, Price = 2 },
+            };
+        }
+
+        public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) return await Task.FromResult(_inventories);
+
+            return _inventories.Where(inventory =>  inventory.InventoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
