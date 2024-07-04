487939d6b2ce7725644bebc2368b58f603adc255
Karl Louis T. Jucutan <kltjucutan@live.mcl.edu.ph>
2024-07-04 15:47:02 +0800

    [Edit Inventory] - Edit Inventory UseCase.

--- IMS.UseCases/PluginInterfaces/IInventoryRepository.cs
+++ IMS.UseCases/PluginInterfaces/IInventoryRepository.cs
@@ -5,6 +5,7 @@ namespace IMS.UseCases.PluginInterfaces
     public interface IInventoryRepository
     {
         Task AddInventoryAsync(Inventory inventory);
+        Task UpdateInventoryAsync(Inventory inventory);
         Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name);
     }
 }
--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddRazorComponents();
 builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
 builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
 builder.Services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase>();
+builder.Services.AddTransient<IEditInventoryUseCase, EditInventoryUseCase>();
 
 var app = builder.Build();
 
