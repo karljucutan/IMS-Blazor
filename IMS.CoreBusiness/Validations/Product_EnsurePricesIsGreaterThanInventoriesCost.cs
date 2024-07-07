6c54708111b6397d6819de6371fecceeaf990d49
Karl Louis T. Jucutan <kltjucutan@live.mcl.edu.ph>
2024-07-07 16:40:28 +0800

    [Product List Add Product] - Create a custom validation attribute.

---
+++ IMS.CoreBusiness/Validations/Product_EnsurePricesIsGreaterThanInventoriesCost.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness.Validations
+{
+    public class Product_EnsurePricesIsGreaterThanInventoriesCost: ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var product = validationContext.ObjectInstance as Product;
+            if (product is not null)
+            {
+                if (!ValidatePricing(product))
+                    return new ValidationResult(
+                        $"The product's price is less than the inventories cost: {TotalInventoriesCost(product).ToString("c")}",
+                        new List<string>() { validationContext.MemberName });
+            }
+
+            return ValidationResult.Success;
+        }
+
+        private double TotalInventoriesCost(Product product)
+        {
+            if (product is null || product.ProductInventories is null) return 0;
+
+            return product.ProductInventories
+                .Sum(productInventory => (productInventory.Inventory?.Price * productInventory.InventoryQuantity) ?? 0);
+        }
+
+        private bool ValidatePricing(Product product)
+        {
+            if (product.ProductInventories is null || product.ProductInventories.Count <= 0) return true;
+
+            if (TotalInventoriesCost(product) > product.Price) return false;
+
+            return true;
+        }
+    }
+}
