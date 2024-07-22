8515f8184ea8a517e49cca77c2b06cefdc9cfa96
Karl Louis T. Jucutan <kltjucutan@live.mcl.edu.ph>
2024-07-22 18:50:30 +0800

    [Reporting] - Product transactions report page.

---
+++ IMS.CoreBusiness/Extensions/ProductTransactionTypeExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace IMS.CoreBusiness.Extensions
+{
+    public static class ProductTransactionTypeExtensions
+    {
+        public static string GetEnumName(this ProductTransactionType type)
+        {
+            string name = type switch
+            {
+                ProductTransactionType.SellProduct => "Sell Product",
+                ProductTransactionType.ProduceProduct => "Produce Product",
+                _ => "Unknown"  // The default case
+            };
+
+            return name;
+        }
+    }
+}
--- IMS.Plugins/IMS.Plugins.InMemory/ProductRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/ProductRepository.cs
@@ -12,7 +12,7 @@ namespace IMS.Plugins.InMemory
             _products = new()
             {
                 new Product { ProductId = 1, ProductName = "Bike", Quantity = 10, Price = 150 },
-                new Product { ProductId = 2, ProductName = "Car", Quantity = 10, Price = 2500 },
+                new Product { ProductId = 2, ProductName = "Car", Quantity = 10, Price = 25000 },
             };
         }
 
