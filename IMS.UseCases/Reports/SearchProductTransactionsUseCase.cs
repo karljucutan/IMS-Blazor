9cf0ce9f001fd811f7a5a946f0e22ff5d19603c5
Karl Louis T. Jucutan <kltjucutan@live.mcl.edu.ph>
2024-07-22 18:08:39 +0800

    [Reporting] - Product transactions usecase.

---
+++ IMS.UseCases/Reports/SearchProductTransactionsUseCase.cs
@@ -0,0 +1,31 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using IMS.UseCases.Reports.Interfaces;
+
+namespace IMS.UseCases.Reports
+{
+    public class SearchProductTransactionsUseCase : ISearchProductTransactionsUseCase
+    {
+        private readonly IProductTransactionRepository _productTransactionRepository;
+
+        public SearchProductTransactionsUseCase(IProductTransactionRepository productTransactionRepository)
+        {
+            _productTransactionRepository = productTransactionRepository;
+        }
+
+        public async Task<IEnumerable<ProductTransaction>> ExecuteAsync(
+            string productName,
+            DateTime? dateFrom,
+            DateTime? dateTo,
+            ProductTransactionType? activityType)
+        {
+            if (dateTo.HasValue) dateTo = dateTo.Value.AddDays(1);
+
+            return await _productTransactionRepository.GetProductTransactionsAsync(
+                productName,
+                dateFrom,
+                dateTo,
+                activityType);
+        }
+    }
+}
