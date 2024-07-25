bf03edaf7e4c738bc4dc0dfc7d2b19ba9806232b
Karl Louis T. Jucutan <kltjucutan@live.mcl.edu.ph>
2024-07-25 16:08:40 +0800

    [Use EF Core] - Product EFCore Repository. InventoryTransaction EFCor eRepository

---
+++ IMS.Plugins/IMS.Plugins.EFCoreSqlServer/Repositories/InventoryTransactionEFCoreRepository.cs
@@ -0,0 +1,77 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMS.Plugins.EFCoreSqlServer.Repositories
+{
+    public class InventoryTransactionEFCoreRepository : IInventoryTransactionRepository
+    {
+        private readonly IDbContextFactory<IMSDbContext> _dbContextFactory;
+
+        public InventoryTransactionEFCoreRepository(IDbContextFactory<IMSDbContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task<IEnumerable<InventoryTransaction>> GetInventoryTransactionsAsync(string inventoryName, DateTime? dateFrom, DateTime? dateTo, InventoryTransactionType? activityType)
+        {
+            using var db = await _dbContextFactory.CreateDbContextAsync();
+
+            var query = (from it in db.InventoryTransactions
+                        join inv in db.Inventories
+                        on it.InventoryId equals inv.InventoryId
+                        where
+                            (string.IsNullOrWhiteSpace(inventoryName) || inv.InventoryName.Contains(inventoryName)) &&
+                            (!dateFrom.HasValue || it.TransactionDate >= dateFrom.Value.Date) &&
+                            (!dateTo.HasValue || it.TransactionDate <= dateTo.Value.Date) &&
+                            (!activityType.HasValue || it.ActivityType == activityType)
+                        select it)
+                        .Include(x => x.Inventory);
+
+            return await query.ToListAsync();
+        }
+
+        // TODO: change to AddProduceProductAsync
+        public async Task ProduceProductAsync(string productionNumber, Inventory inventory, int quantityToConsume, string doneBy, int price)
+        {
+            using var db = await _dbContextFactory.CreateDbContextAsync();
+
+            await db.InventoryTransactions.AddAsync(
+                new InventoryTransaction
+                {
+                    ProductionNumber = productionNumber,
+                    InventoryId = inventory.InventoryId,
+                    QuantityBefore = inventory.Quantity,
+                    ActivityType = InventoryTransactionType.ProduceProduct,
+                    QuantityAfter = inventory.Quantity - quantityToConsume,
+                    TransactionDate = DateTime.UtcNow,
+                    DoneBy = doneBy,
+                    UnitPrice = price
+                });
+
+            await db.SaveChangesAsync();
+        }
+
+        // TODO: change to AddProduceProductAsync
+        public async Task PurchaseInventoryAsync(string purchaseNumber, Inventory inventory, int quantity, string doneBy, double price)
+        {
+            using var db = await _dbContextFactory.CreateDbContextAsync();
+
+            await db.InventoryTransactions.AddAsync(
+                new InventoryTransaction
+                {
+                    PurchaseOrderNumber = purchaseNumber,
+                    InventoryId = inventory.InventoryId,
+                    QuantityBefore = inventory.Quantity,
+                    ActivityType = InventoryTransactionType.PurchaseInventory,
+                    QuantityAfter = inventory.Quantity + quantity,
+                    TransactionDate = DateTime.UtcNow,
+                    DoneBy = doneBy,
+                    UnitPrice = price
+                });
+
+            await db.SaveChangesAsync();
+        }
+    }
+}
+
---
+++ IMS.Plugins/IMS.Plugins.EFCoreSqlServer/Repositories/ProductEFCoreRepository.cs
@@ -0,0 +1,83 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMS.Plugins.EFCoreSqlServer.Repositories
+{
+    // TODO: for enhancements create a base repository and baseentity<Tkey>
+    public class ProductEFCoreRepository : IProductRepository
+    {
+        private readonly IDbContextFactory<IMSDbContext> _dbContextFactory;
+
+        public ProductEFCoreRepository(IDbContextFactory<IMSDbContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task AddProductAsync(Product product)
+        {
+            using var db = await _dbContextFactory.CreateDbContextAsync();
+            await db.Products.AddAsync(product);
+            FlagInventoryUnchanged(product, db);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task DeleteProductByIdAsync(int productId)
+        {
+            using var db = await _dbContextFactory.CreateDbContextAsync();
+            var product = await db.Products.FindAsync(productId);
+            //ArgumentNullException.ThrowIfNull(product);
+            if (product is null) return;
+
+            db.Products.Remove(product);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByNameAsync(string name)
+        {
+            using var db = await _dbContextFactory.CreateDbContextAsync();
+            var test = await db.Products
+                .Where(product => product.ProductName.ToLower().Contains(name.ToLower()))
+                .ToListAsync();
+
+            return test;
+        }
+
+        public async Task<Product> GetProductByIdAsync(int productId)
+        {
+            using var db = await _dbContextFactory.CreateDbContextAsync();
+            var product = await db.Products.FindAsync(productId);
+
+            return product;
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            using var db = await _dbContextFactory.CreateDbContextAsync();
+            var productToUpdate = await db.Products.FindAsync(product.ProductId);
+            //ArgumentNullException.ThrowIfNull(productToUpdate, nameof(productToUpdate));
+            if (productToUpdate is null) return;
+
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.Price = product.Price;
+            productToUpdate.Quantity = product.Quantity;
+            productToUpdate.ProductInventories = product.ProductInventories;
+            FlagInventoryUnchanged(productToUpdate, db);
+
+            await db.SaveChangesAsync();
+        }
+
+        // TODO check if needed or not. check if it will change the inventory record
+        private void FlagInventoryUnchanged(Product product, IMSDbContext dbContext)
+        {
+            if (product?.ProductInventories is not null &&
+                product.ProductInventories.Count > 0)
+            {
+                foreach (var prodInv in product.ProductInventories) 
+                {
+                    dbContext.Entry(prodInv.Inventory).State = EntityState.Unchanged;
+                }
+            }
+        }
+    }
+}
